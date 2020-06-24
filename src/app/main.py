2985f1473cb6aab987e6bc61ed606e5cbb2c6cd3
PiotrIzw <piotr.izworski.0@gmail.com>
2020-06-25 00:23:14 +0200

    Creating menu of application

--- src/app/main.py
+++ src/app/main.py
@@ -2,20 +2,21 @@ import os
 
 from src.app import Player
 
-
-
 while True:
     print("Menu:")
-    print("1. Zawodnik:")
-    print("2. Statystyki:")
-    print("3. Ranking:")
-    print("4. ...")
-    print("9. Wyjście:")
+    print("1. Zawodnik")
+    print("2. Statystyki")
+    print("3. Ranking")
+    print("4. Wyjście")
     what_to_do = input("Wybierz jedną z opcji:\n")
     if what_to_do == '1':
         Player.playerMenu()
-    elif what_to_do == 2:
-       print("...")
-    elif what_to_do == '9':
-       print("Do zobaczenia :)")
-       exit()
+    elif what_to_do == '2':
+        print("...")
+    elif what_to_do == '3':
+        print("...")
+    elif what_to_do == '4':
+        print("Do zobaczenia :)")
+        exit()
+    else:
+        print("Bledny wybor. Sprobuj jeszcze raz.\n")
