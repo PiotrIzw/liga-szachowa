092b85402659bb5735e767f2d2bff39fe0875645
Szymon <simeno10@gmail.com>
2020-06-24 23:20:28 +0200

    2002

--- src/sql/queries.sql
+++ src/sql/queries.sql
@@ -19,12 +19,12 @@ ORDER BY Ranking.Rating DESC
 LIMIT 10;
 
 #3
-#Wybiera maksymalnie 10 zawodnikow wedlug najwiekszej liczby punktow z kategorii JUNIOR powyzej roku urodzenia 2000.
+#Wybiera maksymalnie 10 zawodnikow wedlug najwiekszej liczby punktow z kategorii JUNIOR powyzej roku urodzenia 2002.
 
 SELECT ListaZawodnikow.PlayerID, ListaZawodnikow.FirstName, ListaZawodnikow.Surname, ListaZawodnikow.Country, Ranking.Rating, Ranking.Kategoria
 FROM ListaZawodnikow
 INNER JOIN Ranking ON ListaZawodnikow.PlayerID = Ranking.PlayerID
-WHERE Ranking.Kategoria='JUNIOR' AND ListaZawodnikow.BYear>='2000-01-01'
+WHERE Ranking.Kategoria='JUNIOR' AND ListaZawodnikow.BYear>='2002-01-01'
 ORDER BY Ranking.Rating DESC
 LIMIT 10;
 
